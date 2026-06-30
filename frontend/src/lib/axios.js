1eb45ff2f1d42872f3ac982f1a5b3efbd0185310
Baboucarr Jagne <bmjagne7@gmail.com>
2026-06-30 16:50:03 +0000

    fix: resolve axios base url typo

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : import.meta.env.VITE_API_BASE_URLL;
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
